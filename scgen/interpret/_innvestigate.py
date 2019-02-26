a0fde32882537f4248aec9f8fa115235d59fb401
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-02-26 15:08:50 +0330

    Update interpretability methods

--- scgen/interpret/_innvestigate.py
+++ scgen/interpret/_innvestigate.py
@@ -1,35 +1,27 @@
 import innvestigate
-import numpy as np
-from innvestigate.analyzer import LRPZ, PatternNet, PatternAttribution, SmoothGrad, IntegratedGradients, GuidedBackprop, \
-    Deconvnet
+from innvestigate.analyzer import LRPZ, LRPEpsilon, IntegratedGradients
+
+from scgen.interpret.IntegratedGradients import *
 
 methods = {
-    # "random": Random,
-    # "gradient": Gradient,
-    # "gradient.baseline": BaselineGradient,
-    "deconvnet": Deconvnet,
-    "guided_backprop": GuidedBackprop,
-    "integrated_gradients": IntegratedGradients,
-    "smoothgrad": SmoothGrad,
-
-    # "lrp": LRP,
-    "lrp.z": LRPZ,
+    # "lrp.z": LRPZ,
     # "lrp.epsilon": LRPEpsilon,
-
-    # "deep_taylor": DeepTaylor,
-    "pattern.net": PatternNet,
-    "pattern.attribution": PatternAttribution,
+    "integrated_gradients": IntegratedGradients,
 }
 import os
-
 os.makedirs("./results/", exist_ok=True)
 
 
-def interpret(model, x_data):
+def interpret(model, x_data, neuron_idx=None, save=True):
     for name, method in methods.items():
         analyzer = innvestigate.create_analyzer(name, model, allow_lambda_layers=True)
         gene_analyzes = analyzer.analyze(x_data)
         gene_analyzes = np.array(gene_analyzes)
         gene_analyzes = np.reshape(gene_analyzes, newshape=(-1, x_data.shape[1]))
-        print("%s has finished interpretation!" % name)
-        np.savetxt(fname="./results/%s.txt" % name, X=gene_analyzes, delimiter=",")
+        print(f"{name} for neuron-{neuron_idx} has finished interpretation!")
+        if save:
+            if neuron_idx is not None:
+                np.savetxt(fname=f"./results/{name}_train_{neuron_idx}.txt", X=gene_analyzes, delimiter=",")
+            else:
+                np.savetxt(fname=f"./results/{name}_train_.txt", X=gene_analyzes, delimiter=",")
+        return gene_analyzes
